19e53e1eb24849209072a011600708a4fce02e8b
Trifct <1241525393@qq.com>
2026-07-13 17:41:46 +0800

    Deployed c924a2e with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: false,       // 关闭 $ 转义，避免 # 被误解析
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+});
